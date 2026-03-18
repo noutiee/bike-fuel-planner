753ef8226b0c94b635c2065e9c4f8975d819358d
noutiee <arnoutg93@gmail.com>
2026-03-18 14:57:53 +0100

    Upgrade service worker to version 3

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
 
-const CACHE = 'fuel-planner-v2';
+const CACHE = 'fuel-planner-v3';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/inventory.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',                 // resolves to the project folder on GitHub Pages
+  './index.html',
+  './manifest.json',
+  './inventory.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then((r) => r || fetch(e.request))
   );
 });
